d13d98da3f56a23270ed609652b1f601a8fa0758
Ivan Torriani <150189391+ivantorriani@users.noreply.github.com>
2024-10-26 15:48:18 -0700

    JSON updating entry good but buggy

--- tag_finders/parser.py
+++ tag_finders/parser.py
@@ -10,9 +10,14 @@ with open('HTML_info.json') as f:
 with open('HEADERS.json') as f:
     HEADERS = json.load(f)
 
+with open('Faculty_Information.json') as f:
+    fi = json.load(f)
+
 agent = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
 }
+
+j = 1
 
 # - - - - - - - - - - - - - - - 
 
@@ -31,13 +36,46 @@ headers = table.find_all('td')
 rows = table.find_all("tr")
 
 row_list = []
-for i in rows:
-    data = i.find_all("td")
-    row = [tr.text for tr in data]
-    row_list.append(row)
-    
 
-print(row_list[0])
+def organize_rows():
+    for i in rows:
+        data = i.find_all("td")
+        row = [tr.text for tr in data]
+        row_list.append(row)
+    return row_list
+
+rows = organize_rows()
+
+#print(rows[1][0])
+
+while (j < 12):
+    NAME = (rows[j][0])
+    TITLE = (rows[j][1])
+    EMAIL = (rows[j][2])
+    OFFICE = (rows[j][3])
+    INTERESTS = (rows[j][4])
+
+    entry = {
+        str(NAME):{
+
+            "NAME": str(NAME),
+            "TITLE":  str(TITLE),
+            "EMAIL":  str(EMAIL),
+            "OFFICE": str(OFFICE),
+            "INTERESTS": str(INTERESTS)
+    
+        }
+    }
+    
+    with open('Faculty_Information.json', 'w') as file:
+        json.dump(entry, file, indent=4)
+
+    j += 1
+
+
+
+
+#0 = NAME, #1 = TITLE, #2 = EMAIL, #3 = OFFICE, #4 = RESEARCH INTERESTS
 
 
 
