d1679e1077d67d29372911b79aa257e54ed14082
Ivan Torriani <150189391+ivantorriani@users.noreply.github.com>
2024-11-02 10:34:32 -0700

    Organizing algorithm good. Just need to fix unit gathering

--- part2/p2orgainze.py
+++ part2/p2orgainze.py
@@ -9,15 +9,50 @@ from parse_divs1 import(
 
 # - - - - - - - - - - - - - - - - 
 
+#courseNumber | units | courseTitle | Prerequisite | summary
+
 course_numbers = get_coursenum()
 course_titles = get_course_title()
 course_units = get_course_units()
 course_prereq = get_course_prereq()
 course_description = get_course_description()
 
+official_len = len(course_numbers)
+
 with open('course_info.json') as f:
     COURSE_INFO = json.load(f)
 
 # - - - - - - - - - - - - - - - - 
 
-print(len(course_numbers), len(course_titles), len(course_units), len(course_prereq), len(course_description))
+i = 0
+
+def organize():
+    global i
+    while (i < len(course_numbers)):
+        COURSE_NUM = course_numbers[i]
+        UNITS = course_numbers[i]
+        COURSE_TITLE = course_titles[i]
+        COURSE_PREREQ = course_prereq[i]
+        COURSE_DESC = course_description[i]
+
+        entry = {
+            str(COURSE_TITLE): {
+                "NUMBER": str(COURSE_NUM),
+                "UNITS": str(UNITS),
+                "TITLE": str(COURSE_TITLE),
+                "PREREQUISITE": str(COURSE_PREREQ),
+                "DESCRIPTION": str(COURSE_DESC)
+            }
+        }
+
+        COURSE_INFO.update(entry)
+        i += 1
+    with open('course_info.json', 'w') as file:
+        json.dump(COURSE_INFO, file, indent=4)
+
+
+organize()
+
+
+
+
