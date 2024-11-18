a78037228546531b5241818a626a0b18aa5abd74
Ivan Torriani <150189391+ivantorriani@users.noreply.github.com>
2024-11-18 14:13:14 -0800

    Embedded ge's!

--- embedding/parse_ges/ge_parse.py
+++ embedding/parse_ges/ge_parse.py
@@ -1,7 +1,9 @@
-import os, json
+import os, json, openai
 from halo import Halo as hl
 i = 0 
 # - - - - - - - - - - - - - - - - -
+
+
 
 courses_path = os.path.abspath(
 
@@ -20,6 +22,14 @@ ge_no_desc_path = os.path.abspath(
 
 )
 
+HEADERS_path = os.path.abspath(
+    r"C:\Documents\Computer Projects\AI-For-Data-Ethics\EduScraper\embedding\storage\headers\HEADERS.json"
+)
+
+ge_embeddings_path = os.path.abspath(
+    r"C:\Documents\Computer Projects\AI-For-Data-Ethics\EduScraper\embedding\storage\embeddings\ge_embeddings.json"
+)
+
 
 with open(courses_path) as file:
     courses = json.load(file)
@@ -29,6 +39,15 @@ with open(ge_path) as file:
 
 with open(ge_no_desc_path) as file:
     no_desc_ge = json.load(file)
+
+with open(HEADERS_path) as file:
+    HEADERS = json.load(file)
+
+with open(ge_embeddings_path) as file:
+    ge_embeddings = json.load(file)
+
+openai.api_key = str(HEADERS["AI-Keys"]["OpenAI-3"])
+
 
 
 all_ge_ids = []
@@ -45,14 +64,12 @@ def get_ges_by_id():
     
 id_list = get_ges_by_id()
 
-print(len(id_list))
-print(len(no_desc_ge))
-
 
 def match():
 
     for course in courses:
         i = 0
+        print(len(id_list))
         while (i < len(id_list)):
             if course["id"] == id_list[i]:
                 #des_ids.append(all_ge_ids[i])
@@ -72,8 +89,28 @@ def match():
     with open(ge_path, 'w') as file:
         json.dump(ge, file, indent = 4)
 
-match()
+def embeddings():
+    for course in ge:
+        
+        text_to_vector = openai.embeddings.create(
+            input = str(ge[course]["DESC"]),
+            model = "text-embedding-3-small"
+        )
+
+        entry = {
+            str(course): {
+                "desc_text": str(ge[course]["DESC"]),
+                "desc_vector": text_to_vector.data[0].embedding
+            }
+        }
+
+        ge_embeddings.update(entry)
+
+    with open(ge_embeddings_path, 'w') as file:
+        json.dump(ge_embeddings, file, indent = 4)
 
 
-print(len(ge))
-print(len(no_desc_ge))
+
+embeddings()
+
+
